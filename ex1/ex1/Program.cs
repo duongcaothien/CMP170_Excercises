36cd43e666c490678670f46a7b33ce962df7a723
thien duong <duongcaothien100801hiie>
2021-09-14 19:47:46 +0700

    ex2

---
+++ ex1/ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("input Name : ");
+            String name = Console.ReadLine();
+            Console.Write("Output Name : ");
+            Console.Write(name);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ex2/ex2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input A = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Input B = ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("A + B = ");
+            Console.WriteLine(a + b);
+            Console.Write("A - B = ");
+            Console.WriteLine(a - b);
+            Console.Write("A * B = ");
+            Console.WriteLine(a * b);
+            Console.Write("A / B = ");
+            Console.WriteLine(a / b);
+        }
+    }
+}
