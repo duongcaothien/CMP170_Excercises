ca3455b49ae91cb7f50863e3c933d19d1c6f0461
thien duong <duongcaothien100801hiie>
2021-10-30 13:48:13 +0700

    Practice6

---
+++ Practice6/StudentManagementWithWS/TestStudentWS/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+
+namespace TestStudentWS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44329/api/");
+                var responseTask = client.GetAsync("Students?keyword=test");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    readTask.Wait();
+
+                    Console.WriteLine(readTask.Result);
+                }
+            }
+        }
+    }
+}
---
+++ Practice6/StudentManagementWithWS/TestStudentWS/Student.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace StudentManagementWithWS
+{
+    public class s
+    {
+        public class TestStudentWS
+        {
+            public int studentId { get; set; }
+            public string firstname { get; set; }
+            public string lastname { get; set; }
+            public string email { get; set; }
+            public string gender { get; set; }
+            public string Class { get; set; }
+            public decimal gpa { get; set; }
+        }
+        
+    }
+}
